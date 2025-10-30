76b4aa91d8b61bffed8f16af06c8ba76811b0e2c
Angelos Kamariadis <150456060+akamariadis@users.noreply.github.com>
2025-10-30 21:53:33 +0200

    Update code2.s

--- 1η Σειρά Ασκήσεων/code2.s
+++ 1η Σειρά Ασκήσεων/code2.s
@@ -1,1 +1,22 @@
+BitCount:
+    addi $sp, $sp, -8
+    sw   $ra, 4($sp)
+    sw   $t0, 0($sp)
 
+    beq  $a0, $0, base_case
+
+    andi $t0, $a0, 1
+    srl  $a0, $a0, 1
+    jal  BitCount
+
+    add  $v0, $v0, $t0
+    j    BitCount_end
+
+base_case:
+    li   $v0, 0
+
+BitCount_end:
+    lw   $t0, 0($sp)
+    lw   $ra, 4($sp)
+    addi $sp, $sp, 8
+    jr   $ra
