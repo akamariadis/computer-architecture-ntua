d7604be013c46936aca68e6b682fc51e4be796d9
Angelos Kamariadis <150456060+akamariadis@users.noreply.github.com>
2025-10-30 21:54:07 +0200

    Refactor minimum finding logic in code1A.s

--- 1η Σειρά Ασκήσεων/code1A.s
+++ 1η Σειρά Ασκήσεων/code1A.s
@@ -1,22 +1,22 @@
 min:
-    lw   $t0, 0($a0) # minimum = array[0]
-    li   $t1, 1 # i = 1
+    lw   $t0, 0($a0)
+    li   $t1, 1
 
 loop:
-    bge  $t1, $a1, end # αν i >= size, τερματισμός
-    sll  $t2, $t1, 2 # t2 = i * 4 (offset, επειδή int=4 bytes)
-    add  $t3, $a0, $t2 # διεύθυνση array[i]
-    lw   $t4, 0($t3) # t4 = array[i]
-    blt  $t4, $t0, update # αν array[i] < minimum, πήγαινε στο update
+    bge  $t1, $a1, end
+    sll  $t2, $t1, 2
+    add  $t3, $a0, $t2
+    lw   $t4, 0($t3)
+    blt  $t4, $t0, update
     j    next
 
 update:
-    move $t0, $t4 # minimum = array[i]
+    move $t0, $t4 
 
 next:
-    addi $t1, $t1, 1 # i++
+    addi $t1, $t1, 1
     j    loop
 
 end:
-    move $v0, $t0 # επιστροφή minimum
-    jr   $ra # επιστροφή στον καλούντα
+    move $v0, $t0 
+    jr   $ra
